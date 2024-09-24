5ff82414ccf00eb70890b04f06762c54e90a9c8e
NightmarDark <chibisntb@yandex.ru>
2024-09-24 11:14:02 +0500

    task1 fix

---
+++ Tyuiu.ZaharovVV.Sprint1.Task0.V6.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ZaharovVV.Sprint1.Task0.V6.Lib;
+
+namespace Tyuiu.ZaharovVV.Sprint1.Task0.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void SumValidChecker()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+
+        [TestMethod]
+        public void MultiplieValidChecker()
+        {
+            Assert.AreEqual(10, DataService.Multipie(2, 5));
+        }
+    }
+}
