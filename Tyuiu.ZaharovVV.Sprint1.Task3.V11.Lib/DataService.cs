a99a940fa5b2fe7479f995b3f6837a4b749ed760
NightmarDark <chibisntb@yandex.ru>
2024-09-28 15:21:14 +0500

    111

--- Tyuiu.ZaharovVV.Sprint1.Task3.V11.Lib/DataService.cs
+++ Tyuiu.ZaharovVV.Sprint1.Task3.V11.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.ZaharovVV.Sprint1.Task3.V11.Lib
     {
         public double TriangleArea(double x1, double y1, double x2, double y2, double x3, double y3)
         {
-            return Math.Round(Math.Abs(((x1 - x2) * (y3 - y1)) - ((x3 - x1)* (y2 - y1))) / 2, 3);
+            return Math.Round((Math.Abs(((x2 - x1) * (y3 - y1)) - ((x3 - x1)* (y2 - y1))) / 2), 3);
         }
     }
 }
--- Tyuiu.ZaharovVV.Sprint1.Task3.V11.Test/obj/Debug/net8.0/Tyuiu.ZaharovVV.Sprint1.Task3.V11.Test.AssemblyInfo.cs
+++ Tyuiu.ZaharovVV.Sprint1.Task3.V11.Test/obj/Debug/net8.0/Tyuiu.ZaharovVV.Sprint1.Task3.V11.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tyuiu.ZaharovVV.Sprint1.Task3.V11.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8206bc5b554604ae82547c6cea94e33218aa421")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ad8590c5f74978594f9bb2dcd16c79f349b3e75")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tyuiu.ZaharovVV.Sprint1.Task3.V11.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tyuiu.ZaharovVV.Sprint1.Task3.V11.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
